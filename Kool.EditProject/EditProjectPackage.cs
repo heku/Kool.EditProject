26fc4ccf16e23b91ad697dfb96cec965a536e3b4
heku <heku@sina.cn>
2018-11-12 21:07:57 +0800

    Add VS 16 support

--- Kool.EditProject/EditProjectPackage.cs
+++ Kool.EditProject/EditProjectPackage.cs
@@ -4,36 +4,42 @@ using Microsoft.VisualStudio.Shell;
 using System;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Tasks = System.Threading.Tasks;
 using UIContextGuids = Microsoft.VisualStudio.Shell.Interop.UIContextGuids;
 
 namespace Kool.EditProject
 {
     [Guid(Ids.PACKAGE)]
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.VERSION, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideAutoLoad(Ids.AUTO_LOAD_CONTEXT)]
+    [ProvideAutoLoad(Ids.AUTO_LOAD_CONTEXT, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideUIContextRule(Ids.AUTO_LOAD_CONTEXT,
         "HasProject",
         "SingleProject | MultipleProjects",
         new[] { "SingleProject", "MultipleProjects" },
         new[] { UIContextGuids.SolutionHasSingleProject, UIContextGuids.SolutionHasMultipleProjects },
         delay: 1000)]
-    public sealed class EditProjectPackage : Package
+    public sealed class EditProjectPackage : AsyncPackage
     {
         internal DTE2 DTE { get; private set; }
 
         internal OleMenuCommandService CommandService { get; private set; }
 
-        protected override void Initialize()
+        protected override async Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            DTE = await GetServiceAsync(typeof(EnvDTE.DTE)) as DTE2;
+            CommandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
 
-            DTE = GetService(typeof(EnvDTE.DTE)) as DTE2;
-            CommandService = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
             EditProjectCommand.Initialize(this);
             EditProjectsCommand.Initialize(this);
         }
     }
+
+    // References
+    // https://github.com/Microsoft/VSSDK-Extensibility-Samples/tree/master/VisibilityConstraints
+    // https://github.com/madskristensen/ImageOptimizer/blob/master/src/Helpers/ProjectHelpers.cs
 }
