649d9659cf6649d9d9b9f028387228a381f8b066
heku <heku@sina.cn>
2019-03-24 19:03:36 +0800

    v1.4

--- Kool.EditProject/Vsix.cs
+++ Kool.EditProject/Vsix.cs
@@ -2,7 +2,7 @@
 {
     internal static class Vsix
     {
-        public const string VERSION = "1.3";
+        public const string VERSION = "1.4";
         public const string PRODUCT = "Kool";
         public const string PACKAGE = "Edit Project";
         public const string URL = "https://github.com/heku/kool.editproject";
